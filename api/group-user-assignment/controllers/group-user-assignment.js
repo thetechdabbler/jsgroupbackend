df1273f1de04d3910408a0823bbbec2c2ef54b50
Shubham Dharamkar <dharamkarshubham007@gmail.com>
2020-12-06 21:09:46 +0530

    Modified model relation and controller api for group user assignment

--- api/group-user-assignment/controllers/group-user-assignment.js
+++ api/group-user-assignment/controllers/group-user-assignment.js
@@ -1,28 +1,25 @@
 "use strict";
-
+const { sanitizeEntity } = require('strapi-utils');
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
 
 module.exports = {
-  // create: async (ctx) => {
-  //   if (Array.isArray(ctx.request.body)) {
-  //     //  if array, loop and return created content.
-  //     return "array of content created";
-  //   } else {
-  //     return strapi.services.warehouse.add(ctx.request.body);
-  //   }
-  // },
-
   create: async (ctx) => {
-    if (Array.isArray(ctx.request.body)) {
-      console.log(strapi.services);
-      return await Promise.all(
-        ctx.request.body.map(strapi.services["group-user-assignment"].create)
-      );
-    } else {
-      return strapi.services["group-user-assignment"].create(ctx.request.body);
+    const { assignment_id, group_user_ids } = ctx.request.body;
+    if (Array.isArray(group_user_ids)) {
+      const assignment = await strapi.services["assignment"].findOne({ id: assignment_id });
+      const promises = group_user_ids.map(async (group_user_id) => {
+        const groupUser = await strapi.services['group-user'].findOne({ id: group_user_id });
+        const entity = await strapi.services["group-user-assignment"].create({
+          assignment: assignment,
+          group_user: groupUser
+        });
+        return entity;
+      });
+      const entities = await Promise.all(promises)
+      return entities.map(entity => sanitizeEntity(entity, { model: strapi.models["group-user-assignment"] }));
     }
-  },
-};
+  }
+}
